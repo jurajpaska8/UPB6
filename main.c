816a55cef695a148bc093c99edc50f9993a16f47
jurajpaska8 <jurajpaska8@gmail.com>
2020-11-16 22:09:01 +0100

    sql injection done

--- main.c
+++ main.c
@@ -154,10 +154,140 @@ void format_string_user_input_mitigated()
     printf("Koniec funkcie.");
 }
 
-void sql_injection()
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    int i;
+    for(i = 0; i<argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
+void create_table_user()
 {
     sqlite3 *db;
     int rc = sqlite3_open("test.db", &db);
+    char *zErrMsg = 0;
+
+    if(rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+    else
+    {
+        fprintf(stdout, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    char* sql = "CREATE TABLE USER("  \
+      "ID INT PRIMARY KEY      NOT NULL," \
+      "NAME           TEXT     NOT NULL," \
+      "PWD            CHAR(50) NOT NULL," \
+      "ADDRESS        CHAR(50)," \
+      "SALARY         REAL );";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if( rc != SQLITE_OK ){
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Table created successfully\n");
+    }
+    sqlite3_close(db);
+}
+
+void insert_some_users()
+{
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+
+    /* Open database */
+    rc = sqlite3_open("test.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    /* Create SQL statement */
+    sql = "INSERT INTO USER (ID,NAME,PWD,ADDRESS,SALARY) "  \
+         "VALUES (5, 'Paul', 'heslo1', 'California', 20000.00 ); " \
+         "INSERT INTO USER (ID,NAME,PWD,ADDRESS,SALARY) "  \
+         "VALUES (2, 'Allen', 'heslo2', 'Texas', 15000.00 ); "     \
+         "INSERT INTO USER (ID,NAME,PWD,ADDRESS,SALARY)" \
+         "VALUES (3, 'Teddy', 'heslo3', 'Norway', 20000.00 );" \
+         "INSERT INTO USER (ID,NAME,PWD,ADDRESS,SALARY)" \
+         "VALUES (4, 'Mark', 'heslo4', 'Rich-Mond ', 65000.00 );";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+    if( rc != SQLITE_OK ){
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Records created successfully\n");
+    }
+    sqlite3_close(db);
+}
+
+void sql_injection()
+{
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+    const char* data = "Callback function called";
+
+    /* Open database */
+    rc = sqlite3_open("test.db", &db);
+
+    if( rc ) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return;
+    } else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+
+    char name[40];
+    char pwd[40];
+    printf("Zadaj meno:\n");
+    fgets(name, 40, stdin);
+    printf("Zadaj heslo:\n");
+    fgets(pwd, 40, stdin);
+    char* sql_start = "SELECT * from USER WHERE name='";
+    char* sql_middle = "' and pwd ='";
+    char sql_buf[120];
+    strncpy(sql_buf, sql_start, strlen(sql_start));
+    //printf("%s\n", sql_buf);
+    strncat(sql_buf, name, strlen(name) - 1);
+    //printf("%s\n", sql_buf);
+    strncat(sql_buf, sql_middle, strlen(sql_middle));
+    //printf("%s\n", sql_buf);
+    strncat(sql_buf, pwd, strlen(pwd) - 1);
+    //printf("%s\n", sql_buf);
+    strncat(sql_buf, "'", strlen("'"));
+    printf("%s\n", sql_buf);
+
+    //sql = "SELECT * from USER WHERE name = '' or 1=1 and pwd = '' or 1=1";
+
+    /* Execute SQL statement */
+    rc = sqlite3_exec(db, sql_buf, callback, (void*)data, &zErrMsg);
+
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        sqlite3_free(zErrMsg);
+    } else {
+        fprintf(stdout, "Operation done successfully\n");
+    }
+    sqlite3_close(db);
 }
 
 int main() {
@@ -167,9 +297,9 @@ int main() {
     //basic_overflows();
 
     //buffer overflow
-    printf("Zadaj 30 nahodnych znakov pre explot. Napriklad 123456789123456789123456789123\n");
-    buffer_overflow_elevation_of_privilege();
-    buffer_overflow_elevation_of_privilege_mitigated();
+    //printf("Zadaj 30 nahodnych znakov pre explot. Napriklad 123456789123456789123456789123\n");
+    //buffer_overflow_elevation_of_privilege();
+    //buffer_overflow_elevation_of_privilege_mitigated();
 
     //format string
     //printf("%s", "Zadaj %s%s%s%s%s%s%s%s%s%s%s%s pre zrutenie programu.\n");
@@ -179,7 +309,7 @@ int main() {
     //format_string_user_input_mitigated();
 
     //sql injection
-    //sql_injection();
+    sql_injection();
 
     return 0;
 }
